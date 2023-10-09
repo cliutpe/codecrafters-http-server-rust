df47e8277f75bf69033d2983f8adbc039df44e5a
cliu <cliu@ccliu.me>
2023-10-10 03:00:07 +0800

    Stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use anyhow::Result;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::str;
 
 fn handle_connection(mut stream: TcpStream) {
-    stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+    let mut buffer: [u8; 1024] = [0; 1024];
+    let request_size = stream.read(&mut buffer).unwrap();
+
+    match str::from_utf8(&buffer[..request_size]) {
+        Ok(request_str) => {
+            let lines = request_str.split("\r\n").collect::<Vec<&str>>();
+            let start_line = lines[0].split(" ").collect::<Vec<&str>>();
+            match start_line[1] {
+                r"\" => {
+                    stream.write(b"HTTP/1.1 200 OK").unwrap();
+                }
+                _ => {
+                    stream.write(b"HTTP/1.1 404 Not Found").unwrap();
+                }
+            }
+        }
+        Err(e) => {}
+    }
 }
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
