467ae90353af5d59b574975dfda062381eb524d1
cliu <cliu@ccliu.me>
2023-10-10 19:57:11 +0800

    Stage 7

--- src/main.rs
+++ src/main.rs
@@ -60,12 +60,10 @@ fn handle_connection(mut stream: TcpStream) {
                 );
                 stream.write(buffer.as_bytes()).unwrap();
             } else if request_path.starts_with("/files/") {
-                let dir_arg = env::args().next().unwrap();
-                println!("{:?}", env::args().collect::<Vec<String>>());
-                let dir = dir_arg.split_once("=").unwrap().1;
+                let dir_args = env::args().collect::<Vec<String>>();
 
                 let file_name = request_path.split("/files/").collect::<Vec<&str>>()[1];
-                let file_path = format!("{}/{}", dir, file_name);
+                let file_path = format!("{}/{}", dir_args[dir_args.len()], file_name);
                 println!("{:?}", file_path);
                 match fs::read_to_string(file_path) {
                     Ok(file_content) => {
