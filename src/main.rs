ed74b559b5705db5eb8ca9b67813e8142c7d39c4
cliu <cliu@ccliu.me>
2023-10-10 18:32:03 +0800

    Stage 6

--- src/main.rs
+++ src/main.rs
@@ -29,53 +29,55 @@ fn parse_header(header_str: &str) -> HashMap<&str, &str> {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer: [u8; 1024] = [0; 1024];
-    let request_size = stream.read(&mut buffer).unwrap();
 
-    match str::from_utf8(&buffer[..request_size]) {
-        Ok(request_str) => {
-            println!("{:?}", request_str);
-            let (start_line_str, header_str) = parse_request(request_str);
-            let (_request_method, request_path, _http_version) = parse_start_line(start_line_str);
-            let headers = parse_header(header_str);
+    loop {
+        let request_size = stream.read(&mut buffer).unwrap();
 
-            if request_path == r"/" {
-                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
-            } else if request_path.starts_with("/echo/") {
-                let random_string = request_path.split("/echo/").collect::<Vec<&str>>()[1];
-                let buffer = format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    random_string.len(),
-                    random_string
-                );
-                stream.write(buffer.as_bytes()).unwrap();
-            } else if request_path == "/user-agent" {
-                println!("{:?}", headers);
-                let user_agent = headers["User-Agent"];
-                let buffer = format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                    user_agent.len(),
-                    user_agent
-                );
-                println!("{:?}", buffer);
-                stream.write(buffer.as_bytes()).unwrap();
-            } else {
-                stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+        match str::from_utf8(&buffer[..request_size]) {
+            Ok(request_str) => {
+                println!("{:?}", request_str);
+                let (start_line_str, header_str) = parse_request(request_str);
+                let (_request_method, request_path, _http_version) =
+                    parse_start_line(start_line_str);
+                let headers = parse_header(header_str);
+
+                if request_path == r"/" {
+                    stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                } else if request_path.starts_with("/echo/") {
+                    let random_string = request_path.split("/echo/").collect::<Vec<&str>>()[1];
+                    let buffer = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        random_string.len(),
+                        random_string
+                    );
+                    stream.write(buffer.as_bytes()).unwrap();
+                } else if request_path == "/user-agent" {
+                    println!("{:?}", headers);
+                    let user_agent = headers["User-Agent"];
+                    let buffer = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        user_agent.len(),
+                        user_agent
+                    );
+                    println!("{:?}", buffer);
+                    stream.write(buffer.as_bytes()).unwrap();
+                } else {
+                    stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+                }
             }
+            Err(_e) => {}
         }
-        Err(_e) => {}
     }
 }
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    loop {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(_stream) => {
-                    handle_connection(_stream);
-                }
-                Err(_e) => {}
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                handle_connection(_stream);
             }
+            Err(_e) => {}
         }
     }
     Ok(())
