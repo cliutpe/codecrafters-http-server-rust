4fb344ec8a294da06a53be50db85d59016af10b4
cliu <cliu@ccliu.me>
2023-10-10 18:35:07 +0800

    Stage 6

--- src/main.rs
+++ src/main.rs
@@ -30,43 +30,40 @@ fn parse_header(header_str: &str) -> HashMap<&str, &str> {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer: [u8; 1024] = [0; 1024];
 
-    loop {
-        let request_size = stream.read(&mut buffer).unwrap();
+    let request_size = stream.read(&mut buffer).unwrap();
 
-        match str::from_utf8(&buffer[..request_size]) {
-            Ok(request_str) => {
-                println!("{:?}", request_str);
-                let (start_line_str, header_str) = parse_request(request_str);
-                let (_request_method, request_path, _http_version) =
-                    parse_start_line(start_line_str);
-                let headers = parse_header(header_str);
+    match str::from_utf8(&buffer[..request_size]) {
+        Ok(request_str) => {
+            println!("{:?}", request_str);
+            let (start_line_str, header_str) = parse_request(request_str);
+            let (_request_method, request_path, _http_version) = parse_start_line(start_line_str);
+            let headers = parse_header(header_str);
 
-                if request_path == r"/" {
-                    stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
-                } else if request_path.starts_with("/echo/") {
-                    let random_string = request_path.split("/echo/").collect::<Vec<&str>>()[1];
-                    let buffer = format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                        random_string.len(),
-                        random_string
-                    );
-                    stream.write(buffer.as_bytes()).unwrap();
-                } else if request_path == "/user-agent" {
-                    println!("{:?}", headers);
-                    let user_agent = headers["User-Agent"];
-                    let buffer = format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                        user_agent.len(),
-                        user_agent
-                    );
-                    println!("{:?}", buffer);
-                    stream.write(buffer.as_bytes()).unwrap();
-                } else {
-                    stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
-                }
+            if request_path == r"/" {
+                stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+            } else if request_path.starts_with("/echo/") {
+                let random_string = request_path.split("/echo/").collect::<Vec<&str>>()[1];
+                let buffer = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    random_string.len(),
+                    random_string
+                );
+                stream.write(buffer.as_bytes()).unwrap();
+            } else if request_path == "/user-agent" {
+                println!("{:?}", headers);
+                let user_agent = headers["User-Agent"];
+                let buffer = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    user_agent.len(),
+                    user_agent
+                );
+                println!("{:?}", buffer);
+                stream.write(buffer.as_bytes()).unwrap();
+            } else {
+                stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
             }
-            Err(_e) => {}
         }
+        Err(_e) => {}
     }
 }
 fn main() -> Result<()> {
